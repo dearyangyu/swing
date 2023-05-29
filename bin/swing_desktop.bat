40a4d62590ca3638f03bdabfa81a6f4d872a8dda
Paul Niemandt <paul@wildchildanimation.com>
2023-05-29 14:42:30 +0100

    Fixed path in ENV

--- bin/swing_desktop.bat
+++ bin/swing_desktop.bat
@@ -2,7 +2,7 @@
 
 set WORKING_PATH=%SWING_PATH%
 
-IF NOT DEFINED WORKING_PATH (SET WORKING_PATH=Z:\env\wca)
+IF NOT DEFINED WORKING_PATH (SET WORKING_PATH=C:\WCA)
 
 echo "Running Swing Desktop"
 cd %WORKING_PATH% && %WORKING_PATH%\env\Scripts\activate && %WORKING_PATH%\env\Scripts\python.exe %WORKING_PATH%\swing\swing-main\module\wildchildanimation\plugin\swing_desktop.py
